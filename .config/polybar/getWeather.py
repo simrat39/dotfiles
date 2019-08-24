579349aacf07658dfca74176e185093b15f924f5
simrat39 <simrats169169@gmail.com>
2019-08-24 15:51:09 +0530

    dotfiles: getWeather: Add excpetion handling for no internet
    Signed-off-by: simrat39 <simrats169169@gmail.com>

--- .config/polybar/getWeather.py
+++ .config/polybar/getWeather.py
@@ -2,15 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import html5lib
 
-url = "https://www.timeanddate.com/weather/india/delhi"
-
-soup = BeautifulSoup((requests.get(url)).text, 'html5lib')
-
-weather_test = soup.find(class_="h2")
-
-final_weather = ""
-
-for anything in (weather_test).text.split("\xa0"):
-    final_weather += anything
+try:
+    url = "https://www.timeanddate.com/weather/india/delhi"
+    soup = BeautifulSoup((requests.get(url)).text, 'html5lib')
+    weather_test = soup.find(class_="h2")
+    final_weather = ""
+    for anything in (weather_test).text.split("\xa0"):
+        final_weather += anything
+except requests.exceptions.ConnectionError:
+    final_weather = u'\u2715'
 
 print(final_weather)
