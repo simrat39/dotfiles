93220b6c7c2d82606f34595af5f26838dcc6ed9d
Simrat Singh <simrats169169@gmail.com>
2019-11-20 20:34:50 +0530

    polybar: Update weather script for celcius
    Signed-off-by: Simrat Singh <simrats169169@gmail.com>

--- .config/polybar/getWeather.py
+++ .config/polybar/getWeather.py
@@ -6,10 +6,10 @@ try:
     url = "https://www.timeanddate.com/weather/india/delhi"
     soup = BeautifulSoup((requests.get(url)).text, 'html5lib')
     weather_test = soup.find(class_="h2")
-    final_weather = ""
-    for anything in (weather_test).text.split("\xa0"):
-        final_weather += anything
+    tempra = int(((weather_test).text.split("\xa0"))[0])
+    if "F" in ((weather_test).text.split("\xa0"))[1]:
+        tempra = round((tempra - 32 ) * (5/9))
 except requests.exceptions.ConnectionError:
-    final_weather = ""
+    tempra = ""
 
-print(final_weather)
+print(str(tempra)+"°C")
