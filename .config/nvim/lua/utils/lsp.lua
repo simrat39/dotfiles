825d8dfec6ac5d9dc25393b68428a355fb8264e4
simrat39 <simrats169169@gmail.com>
2021-08-15 13:26:24 -0700

    nvim: lsp: use formatting_sync instead of formatting

--- .config/nvim/lua/utils/lsp.lua
+++ .config/nvim/lua/utils/lsp.lua
@@ -4,7 +4,7 @@ local M = {}
 ---@param command string
 function M.setup_auto_format(ft, command)
     if not command then
-       command = "lua vim.lsp.buf.formatting()"
+       command = "lua vim.lsp.buf.formatting_sync()"
     end
    vim.cmd(string.format("autocmd BufWritePre *.%s %s", ft, command))
 end
